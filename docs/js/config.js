3c9e5813373bd29423c7d8b0578db46802ba07c8
recruitmakrocareer <recruitcpaxtramakro@gmail.com>
2026-05-19 09:20:36 +0700

    Update config.js

--- docs/js/config.js
+++ docs/js/config.js
@@ -1,3 +1,3 @@
 // ตั้งค่า URL ของ Google Apps Script Web App ที่ deploy แล้ว
-const API_URL = 'YOUR_APPS_SCRIPT_WEB_APP_URL_HERE';
+const API_URL = 'https://script.google.com/macros/s/AKfycbwFUgcR1WImqHQj-k-I44KopAAoZGOhigcYTeO9a7aj-SyFqD7RF_KJkrROGPdJs4GRiA/exec';
 // ตัวอย่าง: 'https://script.google.com/macros/s/AKfycb.../exec'
